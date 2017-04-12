19946a40b00f9594ff8428e6ea0d584bc6b3b56b
Garrett Boone <garrettboone@dbc-garrettboone.local>
2017-04-12 11:21:48 -0500

    change module name

--- lib/generators/lightspeed_api/install/install_generator.rb
+++ lib/generators/lightspeed_api/install/install_generator.rb
@@ -1,12 +1,12 @@
 require 'rails/generators'
 require 'rails/generators/migration'
 
-module DoubleDouble
+module LightspeedApi
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
-      desc "Add the migrations for DoubleDouble"
+      desc "Add the migrations for LightspeedApi"
 
       def self.next_migration_number(path)
         next_migration_number = current_migration_number(path) + 1
