d0be75ad5046ecc3f2bc4d125ef1678b3914fb36
Garrett Boone <garrettboone@dbc-garrettboone.local>
2017-04-20 16:06:56 -0500

    add customer findby - it is set to mobile current and raises an error to not use it

--- lib/lightspeed_api/resources/customer.rb
+++ lib/lightspeed_api/resources/customer.rb
@@ -9,6 +9,26 @@ module LightspeedApi
         mfg = {name: manufacturer.name}
         LightspeedCall.make('POST') { HTTParty.post(post_url, body: mfg.to_json, headers: {Authorization: "Bearer #{LightspeedApi::OauthGrant.token}", 'Accept' => 'application/json'}) }
       end
+
+      def find_by_email(email)
+        # TODO change this to email not mobile
+        raise 'This is not setup yet. Change to email before cotinuing to use'
+        find_url = url+'?load_relations=["Contact"]&Contact.mobile=' + email
+        response = LightspeedCall.make('GET') {
+          HTTParty.get(
+              find_url,
+              headers: {Authorization: "Bearer #{LightspeedApi::OauthGrant.token}", 'Accept' => 'application/json'}
+          )}
+      end
+
+      def find_by(query)
+        find_url = url
+        response = LightspeedCall.make('GET') {
+          HTTParty.get(
+              find_url, query: query,
+              headers: {Authorization: "Bearer #{LightspeedApi::OauthGrant.token}", 'Accept' => 'application/json'}
+          )}
+      end
     end
   end
 end
