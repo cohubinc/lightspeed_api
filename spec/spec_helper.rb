e4934b207f7363f0f1788191ae0181c71c9b13f5
Garrett Boone <garrettboone@dbc-garrettboone.local>
2018-02-28 13:40:52 -0600

    remove commented out code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,11 +34,6 @@ RSpec.configure do |config|
   config.before(:each) do
     schema_path = File.join(RAILS_ROOT, 'spec/lightspeed_test_schema.rb')
     NullDB.nullify(:schema => schema_path)
-
-
-    # stub_request(:get, "https://api.merchantos.com/API/Account/accountID/Manufacturer.json").
-    #     with(headers: {'Accept'=>'application/json', 'Authorization'=>'Bearer sometokenhash', 'Content-Type'=>'application/json'}).
-    #     to_return(status: 200, body: "", headers: {'x-ls-api-bucket-level' => ['1/60'],'x-ls-api-drip-rate' => '1'})
   end
 
   config.after(:each) do
