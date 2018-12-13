b3f5c92410878469e056c496fc3f5cfc86617369
Jimmy Baker <jimmyebaker@gmail.com>
2018-12-13 16:09:08 -0600

    wip

--- lib/generators/lightspeed_api/install/templates/add_used_pts_and_level_to_tokens.rb
+++ lib/generators/lightspeed_api/install/templates/add_used_pts_and_level_to_tokens.rb
@@ -1,4 +1,4 @@
-class AddUsedPtsAndLevelToTokens < ActiveRecord::Migration
+class AddUsedPtsAndLevelToTokens < ActiveRecord::Migration[4.2]
   def change
     add_column :access_tokens, :used_points, :integer
     add_column :access_tokens, :bucket_level, :integer
